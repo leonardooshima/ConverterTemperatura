0cd2921b21ea97259821f2bd4251b1a93a6b6567
leonardooshima <leonardooshima@uol.com.br>
2021-01-19 15:09:37 -0300

    Criando Layout

---
+++ app/src/main/java/com/example/convertertemperatura/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.convertertemperatura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun converter(view: View){
+
+    }
+}
