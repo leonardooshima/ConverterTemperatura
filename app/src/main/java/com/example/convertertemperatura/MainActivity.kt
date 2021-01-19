141d93f1734177e1e5c1cba68954818c8b0a56a1
leonardooshima <leonardooshima@uol.com.br>
2021-01-19 15:11:47 -0300

    Recuperando as Views

--- app/src/main/java/com/example/convertertemperatura/MainActivity.kt
+++ app/src/main/java/com/example/convertertemperatura/MainActivity.kt
@@ -9,11 +9,22 @@ import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var editText: EditText
+    lateinit var celsiusRadio: RadioButton
+    lateinit var fahreinheitRadio: RadioButton
+    lateinit var converterButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        editText = findViewById(R.id.valorTemp)
+
+        celsiusRadio = findViewById(R.id.celsiusRadio)
+        fahreinheitRadio = findViewById(R.id.fahreinheitRadio)
+
+        converterButton = findViewById(R.id.converterButton)
     }
 
     fun converter(view: View){
